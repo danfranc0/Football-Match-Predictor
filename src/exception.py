274e09b44012e1bfbacff7716c2a2af32d3d812f
danfranc0 <dfranco1@ufl.edu>
2023-12-28 00:37:39 -0500

    scraping and model training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -13,7 +13,7 @@ def error_message_detail(error, error_detail: sys):
 class CustonException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
