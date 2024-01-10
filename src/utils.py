83ee706ab1392d4e743447ce1dc1d351df7a0d30
danfranc0 <dfranco1@ufl.edu>
2024-01-10 00:42:56 -0500

    hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import numpy as np
 import pandas as pd
@@ -25,7 +26,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
